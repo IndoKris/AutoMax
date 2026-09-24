d4f0d05138efe5ebb1fa7bee9c341f9212e48ed4
Krish Kumar <indokrisgraphics@gmail.com>
2026-09-24 07:34:12 +0530

    Created Login Page
    completed : profile photo routing,profile and location for users

--- automax/urls.py
+++ automax/urls.py
@@ -16,10 +16,16 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include  # added include 
-
+from django.conf import settings
+from django.conf.urls.static import static
 from main import urls as main_app_urls  # added this to connect main urls to automax urls
+from users import urls as users_app_urls # connected users url to project/automax urls
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include(main_app_urls)),
+    path('', include(users_app_urls)),
 ]
+
+if settings.DEBUG :
+    urlpatterns += static(settings.MEDIA_URL,document_root =settings.MEDIA_ROOT)
--- users/models.py
+++ users/models.py
@@ -1,12 +1,34 @@
+from django.conf import settings
+from django.core.validators import RegexValidator
 from django.db import models
-from django.contrib.auth.models import User
+from localflavor.in_.models import INStateField
+from .utils import user_directory_path
+
+class Location(models.Model):
+    address_1 = models.CharField(max_length=128, blank=True)
+    address_2 = models.CharField(max_length=128, blank=True)
+    city = models.CharField(max_length=68)
+    state = INStateField()
+    zip_code = models.CharField(
+        max_length=6,
+        blank=True,
+        validators=[RegexValidator(r'^\d{6}$', 'Enter a 6-digit PIN code.')],
+    )
+
+    def __str__(self):
+        return f"{self.city}, {self.state}"
+
 
 class Profile(models.Model):
-    user = models.OneToOneField(User, on_delete = models.CASCADE)
-    photo = models.ImageField(null= True)
-    bio = models.CharField(max_length=140, blank= True)
-    phone_number = models.CharField(max_length=10, blank= True)
-
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    photo = models.ImageField(upload_to=user_directory_path, null=True, blank=True)
+    bio = models.CharField(max_length=140, blank=True)
+    phone_number = models.CharField(
+        max_length=10,
+        blank=True,
+        validators=[RegexValidator(r'^\d{10}$', 'Enter a 10-digit phone number.')],
+    )
+    location = models.ForeignKey(Location, on_delete=models.SET_NULL, null=True, blank=True)
 
     def __str__(self):
         return f"{self.user.username}'s Profile"
